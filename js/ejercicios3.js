a5e591a4eefeae0ea47ac64361929d85d09651a8
carmen <cmardom599@g.educaand.es>
2023-10-17 12:10:58 +0200

    ej1, 2, 3

--- js/ejercicios3.js
+++ js/ejercicios3.js
@@ -9,7 +9,7 @@ window.addEventListener('load', mover_al_ultimo);
 
 function mover_al_ultimo(){
     let lista = document.getElementById('lista');
-    const firstChild = lista.firstChild;
+    const firstChild = lista.firstElementChild;
     lista.removeChild(firstChild);
     lista.appendChild(firstChild);
 
@@ -21,15 +21,43 @@ function mover_al_ultimo(){
  * DIV.COL dentro del DIV#original, al DIV#copia.
  */
 
-// function copiar(){
-//     const divOrigen = document.getElementById('original');
-//     const divDestino = document.getElementById('copia');
-//
-//     divOrigen.querySelectorAll('div').forEach(function (sourceChild){
-//         const clone = sourceChild.cloneNode(true);
-//
-//         divDestino.appendChild(clone);
-//     }
-// }
+window.addEventListener('load', copiar);
+
+
+function copiar(){
+    const divOrigen = document.getElementById('original');
+    const divDestino = document.getElementById('copia');
+
+    divOrigen.querySelectorAll('div').forEach(function (sourceChild){
+        const clone = sourceChild.cloneNode(true);
+
+        divDestino.appendChild(clone);
+    })
+}
+
+/**
+ * Ejercicio 3
+ * Partiendo del archivo HTML adjunto en carpeta comprimida “actividad3-3”, ordenar los
+ * DIV.COL dentro del propio DIV#ordenar, según el texto que tiene cada DIV.COL.
+ */
+window.addEventListener('load', ordenar);
+
+
+function ordenar(){
+    const divOrdenar = document.getElementById('ordenar');
+    let divs = Array.from(divOrdenar.getElementsByClassName("col"));
+    divs.sort(function (a,b){
+        return a.innerText.localeCompare(b.textContent);
+
+    });
+    divOrdenar.innerHTML = "";
+    divs.forEach(function (div){
+        divOrdenar.appendChild(div)
+    });
+
+}
+
+
+
 
 
